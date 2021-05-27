a37748f6603ff8c41e25c16f0f7a16564883c7ac
TBM85 <tballesterm@gmail.com>
2021-05-27 19:26:46 -0400

    Creates the structure of the flexible panels

---
+++ js/app.js
@@ -0,0 +1,27 @@
+const panelContent = [
+  {class: "panel-one", text: ["One", "I", "Two"]},
+  {class: "panel-two", text: ["Three", "II", "Four"]},
+  {class: "panel-three", text: ["Five", "III", "Six"]},
+  {class: "panel-four", text: ["Seven", "IV", "Eight"]},
+  {class: "panel-five", text: ["Nine", "V", "Ten"]}
+];
+
+const panels = document.querySelector(".panels");
+
+for (let content of panelContent) {
+  const panel = document.createElement("div");
+  panels.appendChild(panel);
+  panel.classList.add("panel", `${content.class}`);
+
+  const firtstParagh = document.createElement("p");
+  panel.appendChild(firtstParagh);
+  firtstParagh.innerHTML = `${content.text[0]}`;
+
+  const secondParagh = document.createElement("p");
+  panel.appendChild(secondParagh);
+  secondParagh.innerHTML = `${content.text[1]}`;
+
+  const thirdParagh = document.createElement("p");
+  panel.appendChild(thirdParagh);
+  thirdParagh.innerHTML = `${content.text[2]}`;
+}
